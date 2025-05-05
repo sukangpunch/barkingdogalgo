128e7da5259755f5f60ad56a213ca5ece249e0d2
sukangpunch <popora99@naver.com>
2025-05-05 18:43:49 +0900

    2025-05-05 스택 괄호문제

---
+++ barkingdogalgo/src/main/java/Stack_parentheses/b_2493.java
@@ -0,0 +1,54 @@
+package Stack_parentheses;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class b_2493 {
+    static class Pair{
+        private int index;
+        private int height;
+        public Pair(int index, int height){
+            this.index = index;
+            this.height = height;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        String []line = br.readLine().split(" ");
+        Stack<Pair> st = new Stack<>();
+        int [] arr = new int[N];
+
+        for(int i = 1; i <= N; i++){
+            int k = Integer.parseInt(line[i-1]);
+            st.push(new Pair(i, k));
+            if(i==1){
+                arr[i-1] = 0;
+            }else{
+                Pair now = st.pop();
+                while(!st.isEmpty()){
+                    if(st.peek().height < now.height){
+                        st.pop();
+                    }else{
+                        break;
+                    }
+                }
+                if(st.isEmpty()){
+                    arr[i-1] = 0;
+                    st.push(now);
+                }else{
+                    arr[i-1] = st.peek().index;
+                    st.push(now);
+                }
+            }
+        }
+        for(int height: arr){
+            sb.append(height).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
---
+++ barkingdogalgo/src/main/java/Stack_parentheses/b_2504.java
@@ -0,0 +1,81 @@
+package Stack_parentheses;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class b_2504 {
+
+    static class Pair{
+        private char parentheses;
+        private int index;
+
+        public Pair(char parentheses, int index){
+            this.parentheses = parentheses;
+            this.index = index;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+        Stack<Pair> pairs = new Stack<>();
+        Stack<int[]> counts = new Stack<>();
+
+        for(int i=0; i<input.length(); i++){
+            char parentheses = input.charAt(i);
+            if(parentheses == '(' || parentheses == '['){
+                pairs.push(new Pair(parentheses, i));
+            }
+            else if(parentheses == ')'){
+                if(pairs.isEmpty() || pairs.peek().parentheses != '('){
+                    System.out.println(0);
+                    return;
+                }
+                else if(i - pairs.peek().index == 1){
+                    counts.push(new int[]{pairs.pop().index, 2});
+                }else{
+                    int tmp = 0;
+                    int size = counts.size();
+                    for(int j=0; j<size; j++){
+                        if(pairs.peek().index < counts.peek()[0]){
+                            tmp += counts.pop()[1];
+                        }
+                    }
+                    counts.push(new int[]{pairs.pop().index, tmp*2});
+                }
+            }else if(parentheses == ']'){
+                if(pairs.isEmpty() || pairs.peek().parentheses != '['){
+                    System.out.println(0);
+                    return;
+                }
+                else if(i - pairs.peek().index == 1){
+                    counts.push(new int[]{pairs.pop().index, 3});
+                }else{
+                    int tmp = 0;
+                    int size = counts.size();
+                    for(int j=0 ; j<size; j++){
+                        if(pairs.peek().index < counts.peek()[0]){
+                            tmp += counts.pop()[1];
+                        }
+                    }
+                    counts.push(new int[]{pairs.pop().index, tmp*3});
+                }
+            }
+        }
+
+        if(!pairs.isEmpty()){
+            System.out.println(0);
+            return;
+        }
+
+        int result = 0;
+
+        while(!counts.isEmpty()){
+            result += counts.pop()[1];
+        }
+        System.out.println(result);
+    }
+}
