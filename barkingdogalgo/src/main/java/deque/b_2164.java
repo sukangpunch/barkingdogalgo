80e1ac523bdd8341c163b612ccddc257e6af69c1
sukangpunch <popora99@naver.com>
2025-11-07 12:33:14 +0900

    2025-11-07 덱 최솟값 찾기 & 카드2 & 덱

---
+++ barkingdogalgo/src/main/java/deque/b_2164.java
@@ -0,0 +1,29 @@
+package deque;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+// 카드2
+public class b_2164 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        Deque<Integer> dq = new ArrayDeque<>();
+
+        for(int i = 1; i <= n; i++){
+            dq.addLast(i);
+        }
+
+        while(dq.size() > 1){
+            dq.removeFirst();
+            dq.addLast(dq.removeFirst());
+        }
+
+        System.out.println(dq.poll());
+    }
+}
