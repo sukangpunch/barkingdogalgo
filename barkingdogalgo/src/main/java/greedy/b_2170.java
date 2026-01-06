a73867a2341b1672cb7539465139edbea2fc7779
sukangpunch <popora99@naver.com>
2026-01-06 12:30:35 +0900

    2026-01-06 그리디, 수학 | 선 긋기, 소수 구하기

---
+++ barkingdogalgo/src/main/java/greedy/b_2170.java
@@ -0,0 +1,79 @@
+package greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+// 선 긋기
+public class b_2170 {
+
+    static class Line{
+        int x;
+        int y;
+
+        public Line(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY(){
+            return y;
+        }
+    }
+
+    static List<Line> points;
+    static int length;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        points = new ArrayList<>();
+        length = 0;
+
+        for(int i=0; i<N; i++){
+            String [] s = br.readLine().split(" ");
+            int x = Integer.parseInt(s[0]);
+            int y = Integer.parseInt(s[1]);
+
+            points.add(new Line(x, y));
+        }
+
+        points.sort(
+                Comparator.comparingInt(Line::getX)
+                        .thenComparing(Line::getY)
+        );
+
+        findLineLength();
+
+        System.out.println(length);
+    }
+
+    private static void findLineLength() {
+        int preEnd = Integer.MIN_VALUE;
+
+        for(int i=0; i<points.size(); i++){
+            Line line = points.get(i);
+            int start = line.x;
+            int end = line.y;
+
+            if(start >= preEnd){
+                length += end - start;
+                preEnd = end;
+            }else if(end <= preEnd){
+                continue;
+            }else{
+                start = preEnd;
+                length += end - start;
+                preEnd = end;
+            }
+        }
+    }
+}
---
+++ barkingdogalgo/src/main/java/math/b_1929.java
@@ -0,0 +1,46 @@
+package math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 소수 구하기
+public class b_1929 {
+
+    static boolean [] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String [] s = br.readLine().split(" ");
+
+        int N = Integer.parseInt(s[0]);
+        int M = Integer.parseInt(s[1]);
+        arr = new boolean[M+1];
+        for(int i = N; i <= M; i++){
+            arr[i] = findPrimeNumber(i);
+        }
+
+        for(int i=N; i<=M; i++){
+            if (arr[i]){
+                sb.append(i).append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+
+    private static boolean findPrimeNumber(int target) {
+        if(target == 1){
+            return false;
+        }
+
+        for(int i = 2; i<= Math.sqrt(target); i++){
+            if(target % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
