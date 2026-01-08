0cd611c9a3df93e2dc5964c22df51a776ddb4826
sukangpunch <popora99@naver.com>
2026-01-08 14:04:14 +0900

    2026-01-08 구현, bfs | 알고 스팟, 마라톤 1

---
+++ barkingdogalgo/src/main/java/dijkstra/b_1261.java
@@ -0,0 +1,88 @@
+package dijkstra;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+// 알고스팟
+public class b_1261 {
+
+    static class Point implements Comparable<Point>{
+        int y;
+        int x;
+        int cnt;
+
+        Point(int y, int x, int cnt){
+            this.y = y;
+            this.x = x;
+            this.cnt = cnt;
+        }
+
+        @Override
+        public int compareTo(Point o) {
+            return cnt - o.cnt;
+        }
+    }
+
+    static int N;
+    static int M;
+    static int [][] map;
+    static boolean [][] visited;
+    static int [][] direction = {{0,1}, {0,-1}, {1,0}, {-1,0}};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String []s = br.readLine().split(" ");
+        N = Integer.parseInt(s[0]);
+        M = Integer.parseInt(s[1]);
+
+        map = new int[M][N];
+        visited = new boolean[M][N];
+
+        for(int i=0; i<M; i++){
+            String str = br.readLine();
+            for(int j=0; j<N; j++){
+                map[i][j] = str.charAt(j) - '0';
+            }
+        }
+
+        int minBreak = findMinBreak();
+        System.out.println(minBreak);
+    }
+
+    private static int findMinBreak() {
+        PriorityQueue<Point> pq = new PriorityQueue<>();
+        pq.offer(new Point(0, 0, 0));
+        visited[0][0] = true;
+
+        while(!pq.isEmpty()){
+            Point now = pq.poll();
+
+            if(now.y == M-1 && now.x == N-1){
+                return now.cnt;
+            }
+
+            for(int i=0; i<4; i++){
+                int dy = now.y + direction[i][0];
+                int dx = now.x + direction[i][1];
+
+                if(dy < 0 || dy >= M || dx < 0 || dx >= N){
+                    continue;
+                }
+
+                if(map[dy][dx] == 0 && !visited[dy][dx]){
+                    visited[dy][dx] = true;
+                    pq.offer(new Point(dy, dx, now.cnt));
+                }
+
+                if(map[dy][dx] == 1 && !visited[dy][dx]){
+                    visited[dy][dx] = true;
+                    pq.offer((new Point(dy,dx, now.cnt + 1)));
+                }
+            }
+        }
+        return 0;
+    }
+}
---
+++ barkingdogalgo/src/main/java/implement/b_10655_wrong.java
@@ -0,0 +1,52 @@
+package implement;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 마라톤 1
+public class b_10655_wrong {
+
+    static int N;
+    static int [][] checkPoint;
+    static int result;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        checkPoint = new int[N][2];
+
+        for(int i=0; i<N; i++){
+            String [] s = br.readLine().split(" ");
+            checkPoint[i][0] = Integer.parseInt(s[0]);
+            checkPoint[i][1] = Integer.parseInt(s[1]);
+        }
+
+        result = Integer.MAX_VALUE;
+        findMinWay();
+
+        System.out.println(result);
+    }
+
+    private static void findMinWay() {
+
+        for(int t = 1; t < N-1 ; t++){
+            int nowResult = 0;
+            int preX = checkPoint[0][0];
+            int preY = checkPoint[0][1];
+
+            for(int i=1; i<N; i++){
+                if(t == i)continue;
+                int nowX = checkPoint[i][0];
+                int nowY = checkPoint[i][1];
+                nowResult += Math.abs(nowX - preX) + Math.abs(nowY - preY);
+
+                preX = nowX;
+                preY = nowY;
+            }
+
+            result = Math.min(result, nowResult);
+        }
+    }
+}
