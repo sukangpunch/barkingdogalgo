7a329ede4eacc19197c293c2469f7c28dd580b54
sukangpunch <popora99@naver.com>
2026-01-09 17:14:19 +0900

    2026-01-09 구현, 다익스트라, 플로이드 | 플로이드, 다익스트라, 오목

---
+++ barkingdogalgo/src/main/java/dijkstra/b_1753.java
@@ -0,0 +1,94 @@
+package dijkstra;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+// 다익스트라
+public class b_1753 {
+
+    static class Node{
+        int end;
+        int weight;
+
+        public Node(int end, int weight){
+            this.end = end;
+            this.weight = weight;
+        }
+    }
+
+    static int V;
+    static int E;
+    static int start;
+    static int [] distance;
+    static boolean [] visited;
+    static List<Node>[] list;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String [] s = br.readLine().split(" ");
+        V = Integer.parseInt(s[0]);
+        E = Integer.parseInt(s[1]);
+        start = Integer.parseInt(br.readLine());
+
+        distance = new int[V+1];
+        visited = new boolean[V+1];
+        list = new List[V+1];
+
+        for(int i=1; i<=V; i++){
+            list[i] = new ArrayList<>();
+            distance[i] = Integer.MAX_VALUE;
+        }
+
+        for(int i=0; i<E; i++){
+            s = br.readLine().split(" ");
+            int start = Integer.parseInt(s[0]);
+            int end = Integer.parseInt(s[1]);
+            int cost = Integer.parseInt(s[2]);
+
+            list[start].add(new Node(end, cost));
+        }
+
+        findShortestWay();
+
+        for(int i=1; i<=V; i++){
+            if(distance[i] == Integer.MAX_VALUE) {
+                sb.append("INF").append("\n");
+            }else{
+                sb.append(distance[i]).append("\n");
+            }
+        }
+        
+        System.out.println(sb);
+    }
+
+    // 우선순위 큐를 통해, 가장 가까운 정점부터 확정
+    // 즉, now 가 (start, 0) 일 때, 인접 방향을 초기화 하기 때문에, 따로 초기화할 필요 없다.
+    private static void findShortestWay() {
+        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> o1.weight - o2.weight);
+        pq.add(new Node(start, 0));
+        distance[start] = 0;
+
+        while(!pq.isEmpty()){
+            Node now = pq.poll();
+
+            if(visited[now.end])continue;
+            visited[now.end] = true;
+
+            for(Node next : list[now.end]){
+                if(visited[next.end]) continue;
+                int newDist = distance[now.end] + next.weight;
+                if(newDist < distance[next.end]){
+                    distance[next.end] = newDist;
+                    pq.add(new Node(next.end, newDist));
+                }
+            }
+        }
+    }
+
+}
---
+++ barkingdogalgo/src/main/java/floyd/b_11404.java
@@ -0,0 +1,71 @@
+package floyd;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 플로이드
+public class b_11404 {
+
+    static int N;
+    static int M;
+    static int [][] map;
+
+    static final int MAX = 987654321;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+        map = new int[N+1][N+1];
+
+        for(int i=1; i<=N; i++){
+            for(int j=1; j<=N; j++){
+                if(i==j) map[i][j] = 0;
+                else{
+                    map[i][j] = MAX;
+                }
+            }
+        }
+
+        for(int i=0; i<M; i++){
+            String [] s = br.readLine().split(" ");
+
+            int start = Integer.parseInt(s[0]);
+            int end = Integer.parseInt(s[1]);
+            int cost = Integer.parseInt(s[2]);
+            map[start][end] = Math.min(map[start][end], cost);
+        }
+        findAllVertexShortestWay();
+
+        for(int i=1; i<=N; i++){
+            for(int j=1; j<=N; j++){
+                if(map[i][j]== MAX){
+                    sb.append(0).append(" ");
+                }else{
+                    sb.append(map[i][j]).append(" ");
+                }
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    private static void findAllVertexShortestWay() {
+        for(int k=1; k<=N; k++){
+            for(int i=1; i<=N; i++){
+                if(map[i][k] == MAX) continue;
+                for(int j=1; j<=N; j++){
+                    if(map[k][j] == MAX)continue;
+
+                    if(map[i][j] > map[i][k] + map[k][j]){
+                        map[i][j] = map[i][k] + map[k][j];
+                    }
+                }
+            }
+        }
+    }
+}
