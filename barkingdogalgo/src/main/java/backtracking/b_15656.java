3da1d0235d2e872575d7440d7950bec1f6a14902
sukangpunch <popora99@naver.com>
2026-01-10 16:03:09 +0900

    2026-01-10 구현, 백트래킹 | N과M(7,8) 괄호의 값

---
+++ barkingdogalgo/src/main/java/backtracking/b_15656.java
@@ -0,0 +1,56 @@
+package backtracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// N과 M (7)
+public class b_15656 {
+
+    static int [] nums;
+    static int N;
+    static int M;
+    static StringBuilder sb;
+    static List<Integer> tmp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        tmp = new ArrayList<>();
+
+        String []s = br.readLine().split(" ");
+        N = Integer.parseInt(s[0]);
+        M = Integer.parseInt(s[1]);
+
+        s = br.readLine().split(" ");
+        nums = new int[N];
+        for(int i=0; i<N; i++){
+            nums[i] = Integer.parseInt(s[i]);
+        }
+
+        Arrays.sort(nums);
+
+        backtracking(0);
+
+        System.out.println(sb);
+    }
+
+    private static void backtracking(int depth) {
+        if(depth == M){
+            int size = tmp.size();
+            for(int i=0; i<size; i++){
+                sb.append(tmp.get(i)).append(" ");
+            }
+            sb.append("\n");
+        }else{
+            for(int i=0; i<N; i++){
+                tmp.add(nums[i]);
+                backtracking(depth + 1);
+                tmp.remove(tmp.size()-1);
+            }
+        }
+    }
+}
---
+++ barkingdogalgo/src/main/java/implement/b_2504.java
@@ -0,0 +1,58 @@
+package implement;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+// 괄호의 값
+public class b_2504 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        char[] parent = br.readLine().toCharArray();
+        Stack<Character> st = new Stack<>();
+        int temp = 1;
+        int result = 0;
+        int size = parent.length;
+
+        for(int i=0; i<size; i++){
+            char now = parent[i];
+
+            if(now == '(' || now == '['){
+                st.push(now);
+                temp *= now=='(' ? 2 : 3;
+            }
+            else if(now == ')'){
+                if(st.empty() || st.peek() != '('){
+                    result = 0;
+                    break;
+                }
+
+                if(parent[i-1] == '('){
+                    result += temp;
+                }
+                temp /=2;
+                st.pop();
+            }
+            else if(now == ']'){
+                if(st.empty() || st.peek() != '['){
+                    result = 0;
+                    break;
+                }
+
+                if(parent[i-1] == '['){
+                    result += temp;
+                }
+                temp /=3;
+                st.pop();
+            }
+        }
+        if(!st.empty()){
+            result = 0;
+        }
+
+        System.out.println(result);
+    }
+}
